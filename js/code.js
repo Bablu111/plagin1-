a1e95a632449513504abc9b7cbc312d80e4474c5
Bablu111 <103333242+Bablu111@users.noreply.github.com>
2022-07-04 03:19:53 +0600

    hw

---
+++ js/code.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $(".slider").slick({
+
+        slideToShow: 3,
+        slideToScroll: 3,
+        
+        centerMode: true,
+        dots: false,
+        prevArrow: ".left",
+        nextArrow: ".right",
+
+
+    });
+});
